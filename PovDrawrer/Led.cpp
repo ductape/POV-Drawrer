c0dc9c6044135ec9b162561488f7c822bb3ae784
spgreene <spgreene@indesign-llc.com>
2014-11-25 20:36:38 -0500

    Simplified the LED class.

--- PovDrawrer/Led.cpp
+++ PovDrawrer/Led.cpp
@@ -1,34 +1,23 @@
 #include "Led.h"
 #include <QtWidgets>
-#include <QScreen>
 
 static const qreal LED_SIZE_IN = 0.1259;
 
+/**
+ * @brief Led::Led creates a QGraphicsEllipseItem that is set with the defaults of a special LED type.
+ * @param color - the color to set the LED to.
+ * @param x - the x coordinate for the center position of the LED
+ * @param y - the y coordinate for hte center position of the LED
+ */
 Led::Led(const QColor color, int x, int y) :
-    x(x),
-    y(y),
-    color(color),
     dpi(102.0),
     sizeInches(LED_SIZE_IN)
 {
-    setZValue((x + y) % 2);
-    setFlags(ItemIsSelectable | ItemIsMovable);
-    setAcceptHoverEvents(true);
-}
+    qreal diameter = this->dpi * this->sizeInches;
 
-QRectF Led::boundingRect() const
-{
-    qreal height = this->dpi * LED_SIZE_IN;
-    qreal width = this->dpi * LED_SIZE_IN;
-
-    return QRectF(width/2, -height/2, width, height);
-}
-
-QPainterPath Led::shape() const
-{
-    QPainterPath path;
-    path.addEllipse(this->boundingRect());
-    return path;
+    this->setRect(QRectF((diameter/2) + x, (-diameter/2) - y, diameter, diameter));
+    this->setPen(QPen(color.darker()));
+    this->setBrush(QBrush(color));
 }
 
 void Led::setScreenDpi(qreal dpi)
@@ -49,51 +38,4 @@ void Led::setLedSizeInches(qreal inches)
 qreal Led::ledSizeInches() const
 {
     return this->sizeInches;
-}
-
-void Led::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
-{
-    Q_UNUSED(widget);
-
-    QColor fillColor = (option->state & QStyle::State_Selected) ? color.dark(150) : color;
-    if (option->state & QStyle::State_MouseOver)
-    {
-        fillColor = fillColor.light(125);
-    }
-
-    QPen oldPen = painter->pen();
-    QPen pen = oldPen;
-    int width = 0;
-    if (option->state & QStyle::State_Selected)
-    {
-        width += 2;
-    }
-
-    pen.setWidth(width);
-    QBrush b = painter->brush();
-    painter->setBrush(QBrush(fillColor.dark(option->state & QStyle::State_Sunken ? 120 : 100)));
-
-    painter->drawEllipse(QRectF(this->boundingRect().width()/2,
-                                -this->boundingRect().height()/2,
-                                this->boundingRect().width(),
-                                this->boundingRect().height()));
-    painter->setBrush(b);
-}
-
-void Led::mousePressEvent(QGraphicsSceneMouseEvent *event)
-{
-    QGraphicsItem::mousePressEvent(event);
-    update();
-}
-
-void Led::mouseMoveEvent(QGraphicsSceneMouseEvent *event)
-{
-    QGraphicsItem::mouseMoveEvent(event);
-    update();
-}
-
-void Led::mouseReleaseEvent(QGraphicsSceneMouseEvent *event)
-{
-    QGraphicsItem::mouseReleaseEvent(event);
-    update();
 }
--- PovDrawrer/Led.h
+++ PovDrawrer/Led.h
@@ -2,30 +2,19 @@
 #define LED_H
 
 #include <QColor>
-#include <QGraphicsItem>
+#include <QGraphicsEllipseItem>
 
-class Led : public QGraphicsItem
+class Led : public QGraphicsEllipseItem
 {
 public:
-    Led(const QColor color, int x, int y);
+    explicit Led(const QColor color = Qt::red, int x = 0, int y = 0);
 
-    QRectF boundingRect() const;
-    QPainterPath shape() const;
     void setScreenDpi(qreal dpi);
     qreal screenDpi() const;
     void setLedSizeInches(qreal inches);
     qreal ledSizeInches() const;
-    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
-
-protected:
-    void mousePressEvent(QGraphicsSceneMouseEvent *event);
-    void mouseMoveEvent(QGraphicsSceneMouseEvent *event);
-    void mouseReleaseEvent(QGraphicsSceneMouseEvent *event);
 
 private:
-    int x;
-    int y;
-    QColor color;
     qreal dpi;
     qreal sizeInches;
 };
